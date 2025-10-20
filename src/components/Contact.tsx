badf553e6ab47e56064711da0956adbe86ba0831
Gokulakrishnan-maker <gokulakrishnan756@gmail.com>
2025-10-20 09:02:55 +0530

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,7 +6,7 @@ const Contact = () => {
     {
       icon: Phone,
       title: 'Call Us',
-      details: '+91 7305470462',
+      details: '+91 6382980204',
       description: '24/7 booking and support'
     },
     {
